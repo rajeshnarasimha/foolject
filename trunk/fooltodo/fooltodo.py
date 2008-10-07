c18eec3f46e62671e7bd9a1045cac02a95295523
greatfoolbear <greatfoolbear@756bb6b0-a119-0410-8338-473b6f1ccd30>
2008-10-07 05:50:54 +0000

    使用用户服务 http://code.google.com/intl/zh-CN/appengine/docs/gettingstarted/usingusers.html

--- trunk/fooltodo/fooltodo.py
+++ trunk/fooltodo/fooltodo.py
@@ -1,11 +1,21 @@
 import wsgiref.handlers
 
+from google.appengine.api import users
 from google.appengine.ext import webapp
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write('Hello, webapp World!')
+    user = users.get_current_user()
+
+    if user:
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Hello, ' + user.nickname() + ', and your e-mail is ' + user.email() + '.')
+      admin = users.is_current_user_admin()
+      if admin:
+      	self.response.out.write('\nYou are administrator.')
+      self.response.out.write('\n <a ref=\'' + users.create_logout_url(self.request.uri) + '\'>Logout</a>')
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
 
 def main():
   application = webapp.WSGIApplication(
