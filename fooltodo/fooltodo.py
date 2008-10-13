790e05d65513d3e608df0de889e5a926408ca7cf
greatfoolbear <greatfoolbear@756bb6b0-a119-0410-8338-473b6f1ccd30>
2008-10-13 06:16:40 +0000

    1.email attachments 2.email html body

--- fooltodo/fooltodo.py
+++ fooltodo/fooltodo.py
@@ -18,9 +18,9 @@ class Greeting(db.Model):
 class MainPage(webapp.RequestHandler):
   def get(self):
     #greetings_query = Greeting.all().order('-date')
-    #greetings = greetings_query.fetch(10)
-    greetings = db.GqlQuery("Select * from Greeting Order by date DESC limit 10")
-    #greetings = db.GqlQuery("Select * from Greeting where author=:author Order by date DESC limit 10", author=users.get_current_user())
+    #greetings = greetings_query.fetch(6)
+    greetings = db.GqlQuery("Select * from Greeting Order by date DESC limit 6")
+    #greetings = db.GqlQuery("Select * from Greeting where author=:author Order by date DESC limit 6", author=users.get_current_user())
     if users.get_current_user():
       url = users.create_logout_url(self.request.uri)
       url_linktext = 'Logout'
@@ -38,28 +38,40 @@ class MainPage(webapp.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
 class Guestbook(webapp.RequestHandler):
-  def post(self):
+  def post(self):      
     greeting = Greeting()
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+    current_user = users.get_current_user()
+    if current_user:
+      greeting.author = current_user
     greeting.content = self.request.get('content')
     image = self.request.get("img")
     if image:
-      avatar =  images.resize(image, 32, 32)
+      avatar = images.resize(image, 32, 32)
       greeting.avatar = db.Blob(avatar)
     greeting.put()
-    if users.get_current_user():
+    if current_user:        
       message = mail.EmailMessage()
-      message.sender = users.get_current_user().email()
+      message.sender = current_user.email()
       message.to = "greatfoolbear@gmail.com"
       message.subject = "Message left in your guest book"
       message.body = """
-Dear Administrator,
-
-%s Leave a message in your guest book. Please visit http://fooltodo.appspot.com to check it out.
-
-Fool To-do Team
-""" % users.get_current_user().nickname()
+ Dear Administrator,
+ 
+ User %s Leave a message in your guest book. Please visit http://fooltodo.appspot.com to check it out.
+ "%s"
+ 
+ Fool To-do Team
+ """ % (current_user.nickname(), greeting.content)
+      message.html = """
+ Dear Administrator,<br/><br/>
+ 
+ User <b>%s</b> Leave a message in your guest book. Please visit <a href="http://fooltodo.appspot.com"><b>HERE</b></a> to check it out.<br/>
+ <I>"%s"</I><br/><br/>
+ 
+ Fool To-do Team
+ """ % (current_user.nickname(), greeting.content)
+      if image:
+        message.attachments = [("avatar.jpg", greeting.avatar)]
       message.send()
     self.redirect('/')
     
